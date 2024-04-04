f2e27393becd515449afbb10aec6b835911572b4
Leonardo Cardozo <leonardo.rc999@gmail.com>
2024-04-03 21:14:09 -0300

    feat: ending microservice quotation

--- cotacao/src/main/java/br/com/mineradorabr/dto/CurrencyPriceDTO.java
+++ cotacao/src/main/java/br/com/mineradorabr/dto/CurrencyPriceDTO.java
@@ -1,14 +1,14 @@
 package br.com.mineradorabr.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 @Jacksonized
 @Data
-@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyPriceDTO {
     public USDBRL USDBRL;
 }
